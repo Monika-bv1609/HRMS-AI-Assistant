a91e86b670d671d0498c9998b19c3715b9d79bdb
Monika BV <monikavishu1609@gmail.com>
2026-05-26 13:27:58 +0530

    mutiple pdf processed successfully

--- backend/api/upload.py
+++ backend/api/upload.py
@@ -1,11 +1,13 @@
+from typing import List
+
 from fastapi import APIRouter, UploadFile, File
 
-from services.upload_service import upload_pdf_to_rag
+from services.upload_service import upload_multiple_pdfs_to_rag
 
 router = APIRouter()
 
 
 @router.post("/upload-pdf")
-def upload_pdf(file: UploadFile = File(...)):
+def upload_pdf(files: List[UploadFile] = File(...)):
 
-    return upload_pdf_to_rag(file)
+    return upload_multiple_pdfs_to_rag(files)
--- backend/services/upload_service.py
+++ backend/services/upload_service.py
@@ -3,17 +3,31 @@ import requests
 from utils.config import PDF_AGENT_URL
 
 
-def upload_pdf_to_rag(file):
+def upload_multiple_pdfs_to_rag(files):
 
+    uploaded_files = []
 
-# multipart/form-data upload
-    files = {
-        "file": (file.filename, file.file, file.content_type)
+    for file in files:
+
+        file_data = {
+            "file": (
+                file.filename,
+                file.file,
+                file.content_type
+            )
+        }
+
+        response = requests.post(
+            f"{PDF_AGENT_URL}/read-pdf",
+            files=file_data
+        )
+
+        uploaded_files.append({
+            "filename": file.filename,
+            "response": response.json()
+        })
+
+    return {
+        "message": "All PDFs processed successfully",
+        "uploaded_files": uploaded_files
     }
-
-    response = requests.post(
-        f"{PDF_AGENT_URL}/read-pdf",
-        files=files
-    )
-
-    return response.json()
