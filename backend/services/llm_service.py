41a28a3fd4e48766f12d934f7f378b576312ff6c
Monika BV <monikavishu1609@gmail.com>
2026-05-29 13:01:35 +0530

    llm added

---
+++ backend/services/llm_service.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+def classify_intent(question):
+
+    prompt = f"""
+Return ONLY one of:
+
+employee_count
+leave_today
+employee_search
+
+Question:
+{question}
+"""
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0
+    )
+
+    return (
+        response
+        .choices[0]
+        .message
+        .content
+        .strip()
+    )
