f04763cedfd1b051676905285d371caa066dcd67
Monika BV <monikavishu1609@gmail.com>
2026-05-29 10:40:35 +0530

    feat: integrate Odoo employee API

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.chat import router as chat_router
 from api.upload import router as upload_router
+from api.odoo import router as odoo_router
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ def home():
 
 app.include_router(chat_router)
 app.include_router(upload_router)
+app.include_router(odoo_router)
